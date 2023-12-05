511c5c918094ed195a84ca555e3c7819d58fb5f6
victorparkh <44875239+victorparkh@users.noreply.github.com>
2023-12-05 23:33:53 +0200

    Update content.js - deleted comments

--- content.js
+++ content.js
@@ -1,38 +1,4 @@
 
-// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-//   if (request.action === 'parseHeaders') {
-//     const h1Headings = Array.from(document.querySelectorAll('h1'))
-//       .map(header => header.innerText.trim())
-//       .filter(textContent => textContent.length > 0);
-
-//     const h2Headings = Array.from(document.querySelectorAll('h2'))
-//       .map(header => header.innerText.trim())
-//       .filter(textContent => textContent.length > 0);
-
-//     const h3Headings = Array.from(document.querySelectorAll('h3'))
-//       .map(header => header.innerText.trim())
-//       .filter(textContent => textContent.length > 0);
-
-//     chrome.runtime.sendMessage({
-//       action: 'updateHeadersData',
-//       headersData: {
-//         h1Headings: h1Headings,
-//         h2Headings: h2Headings,
-//         h3Headings: h3Headings
-//       }
-//     });
-
-//     chrome.runtime.sendMessage({
-//       action: 'displayHeadings',
-//       h1Headings: h1Headings,
-//       h2Headings: h2Headings,
-//       h3Headings: h3Headings
-//     });
-//   }
-// });
-
-
-// content.js
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.action === 'parseHeaders') {
