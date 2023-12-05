5aa9f40a6d94efe94995c0bb1f33058b5eb5fc6f
victorparkh <44875239+victorparkh@users.noreply.github.com>
2023-12-05 23:34:15 +0200

    Update popup.js - deleted comments

--- popup.js
+++ popup.js
@@ -1,22 +1,4 @@
 
-// document.addEventListener('DOMContentLoaded', function () {
-//   document.getElementById('parseBtn').addEventListener('click', function () {
-//     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-//       chrome.tabs.sendMessage(tabs[0].id, { action: 'parseHeaders' });
-//     });
-//   });
-
-//   chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-//     if (request.action === 'displayHeadings') {
-//       document.getElementById('resulth1').innerText = request.h1Headings.join(';\n ') || 'No H1 headers found on the page.';
-//       document.getElementById('resulth2').innerText = request.h2Headings.join(';\n ') || 'No H2 headers found on the page.';
-//       document.getElementById('resulth3').innerText = request.h3Headings.join(';\n ') || 'No H3 headers found on the page.';
-//     }
-//   });
-// });
-
-
-// popup.js
 
 document.addEventListener('DOMContentLoaded', function () {
   document.getElementById('parseBtn').addEventListener('click', function () {
